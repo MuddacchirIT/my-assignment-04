ad5d348529ea2a4716792cc071c4d6fa35adfe46
unknown <protexbeer41@gmail.com>
2025-08-15 19:30:31 +0600

    Assignment Submitted on right timed

--- assignment04.js
+++ assignment04.js
@@ -1,20 +1,20 @@
                
                
                
-               // 13 August 2025  Deadlin 15 August 2025
+               // My Assignment 04
 // -----------------------------------------------------------
 // Assignment 04 started here
 // Problem 01 here
 
-//  function totalFine(fare){
-//      if(fare <= 0 || typeof fare != "number"){
-//         return "Invalid"} 
+ function totalFine(fare){
+     if(fare <= 0 || typeof fare != "number"){
+        return "Invalid"} 
 
-//     let surcharge = 0.2;
-//     let extraServiceCharge = 30;
-//     let fine = fare + (fare * surcharge) + extraServiceCharge;
-//     return fine;
-//     }
+    let surcharge = 0.2;
+    let extraServiceCharge = 30;
+    let fine = fare + (fare * surcharge) + extraServiceCharge;
+    return fine;
+    }
 
 // const result = totalFine(200);
 // console.log(result);
@@ -22,16 +22,16 @@
 // -----------------------------------------------------------
 // Problem 02 here
 
-//  function onlyCharacter(str){ 
-//     if(typeof str != "string"|| Array.isArray ==="str"||typeof str.includes === Boolean){
-//         return "Invalid"
-//      }
+ function onlyCharacter(str){ 
+    if(typeof str != "string"|| Array.isArray ==="str"||typeof str.includes === Boolean){
+        return "Invalid"
+     }
 
-//     else{let upperStr = str.toUpperCase();
-//         let noSpaceStr = upperStr.split(" ").join("");
-//         return noSpaceStr;
-//      }  
-//  }
+    else{let upperStr = str.toUpperCase();
+        let noSpaceStr = upperStr.split(" ").join("");
+        return noSpaceStr;
+     }  
+ }
  
 // const result = onlyCharacter("    h  e  llo   wor  ld");
 // console.log(result);
@@ -42,32 +42,32 @@
 // let team1 = {name:"Brazil",foul:5,cardY:1,cardR:0};
 // let team2 = {name:"Argentina",foul:7,cardY:0,cardR:0};
 
-//  function bestTeam(player1, player2){
-//     if(Array.isArray(player1)||Array.isArray(player2)|| typeof player1 ===   "string" || typeof player2 === "string"){
-//         return "Invalid"
-//     }
-//     let players1Sum = 0;
-//     for(let key in player1){
-//         if(typeof player1[key] === "number"){
-//             players1Sum += player1[key]
-//         }
-//     }
-//     let players2Sum = 0;
-//       for(let key in player2){
-//         if(typeof player2[key] === "number"){
-//             players2Sum += player2[key]
-//         }
-//     }
-//     if(players1Sum < players2Sum){
-//         return `${player1.name}`
+ function bestTeam(player1, player2){
+    if(Array.isArray(player1)||Array.isArray(player2)|| typeof player1 ===   "string" || typeof player2 === "string"){
+        return "Invalid"
+    }
+    let players1Sum = 0;
+    for(let key in player1){
+        if(typeof player1[key] === "number"){
+            players1Sum += player1[key]
+        }
+    }
+    let players2Sum = 0;
+      for(let key in player2){
+        if(typeof player2[key] === "number"){
+            players2Sum += player2[key]
+        }
+    }
+    if(players1Sum < players2Sum){
+        return `${player1.name}`
 
-//     }
-//     else if(players1Sum > players2Sum){
-//         return `${player2.name}`
-//     }
-//     else{
-//          return "Tie"}
-//     }
+    }
+    else if(players1Sum > players2Sum){
+        return `${player2.name}`
+    }
+    else{
+         return "Tie"}
+    }
 
 //  const result = bestTeam(team1, team2);
 //  console.log(result);
@@ -78,27 +78,27 @@
 // let a = [1, 2, 3];
 // let b = [1, 2, 3];
 
-//  function isSame(arr1,arr2){ 
-//     if(Array.isArray(arr1) !== true || Array.isArray(arr2) !== true){
-//        return "Invalid";
-//     } 
-//    else if(Array.isArray(arr1) || Array.isArray(arr2) || typeof arr1.includes === "number" || typeof arr2.includes === "number"){
-//       for(let i = 0; i <= arr2.length - arr1.length; i++){
-//          let same = true;
-//          for (let k = 0; k < arr1.length; k++){
-//             if(arr1[k] !== arr2[i+k]){
-//                same = false;
-//                break;
-//             }
-//          }
-//          if (same) return true;
-//       }
-//       return false;
-//    }
-//    else{
-//       return false;
-//    }
-//  }
+ function isSame(arr1,arr2){ 
+    if(Array.isArray(arr1) !== true || Array.isArray(arr2) !== true){
+       return "Invalid";
+    } 
+   else if(Array.isArray(arr1) || Array.isArray(arr2) || typeof arr1.includes === "number" || typeof arr2.includes === "number"){
+      for(let i = 0; i <= arr2.length - arr1.length; i++){
+         let same = true;
+         for (let k = 0; k < arr1.length; k++){
+            if(arr1[k] !== arr2[i+k]){
+               same = false;
+               break;
+            }
+         }
+         if (same) return true;
+      }
+      return false;
+   }
+   else{
+      return false;
+   }
+ }
 
 //  const result = isSame(a, b);
 //  console.log(result);
@@ -106,7 +106,7 @@
 // -----------------------------------------------------------
 // Problem 05 here
  
-const input = [];
+// const input = [];
 
 function resultReport(marks){ 
    if(Array.isArray(marks) === false){
@@ -145,4 +145,4 @@ const result = resultReport(input);
 console.log(result);
 
 
-               //------END------
+            //------END------
